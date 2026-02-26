ad1a917a35ef46c611fb5508cf43630c31b69565
Jose Hernandez <jdhernandezmercado@unicesar.edu.co>
2026-02-26 18:21:31 -0500

    feat: agregar constructor y encapsulamienta a clases libro

--- src/main/java/modelos/Libro.java
+++ src/main/java/modelos/Libro.java
@@ -28,6 +28,90 @@ public class Libro {
   
   public Libro(){
   }
+
+    /**
+     * @return the isbn
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the autor
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    /**
+     * @return the genero
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    /**
+     * @return the anioPublicacion
+     */
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    /**
+     * @param anioPublicacion the anioPublicacion to set
+     */
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    /**
+     * @return the disponible
+     */
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    /**
+     * @param disponible the disponible to set
+     */
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
       
 }
   
---
+++ src/main/java/persistencia/LibroRepository.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persistencia;
+import java.util.ArrayList;
+import modelos.Libro;
+/**
+ *
+ * @author HP
+ */
+public class LibroRepository {
+     private final LibroRepository repository;
+     
+     public LibroRepository(){
+         this.repository = new LibroRepository();
+     }
+     
+     public void crearLibro(Libro libro){
+         repository.crearLibro(libro);
+     }
+ }   
+
