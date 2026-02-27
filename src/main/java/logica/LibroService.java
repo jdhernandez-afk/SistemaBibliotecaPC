84e59f4665753c578cee72815097ab4ddb276a51
Jose Hernandez <jdhernandezmercado@unicesar.edu.co>
2026-02-26 20:38:51 -0500

    feat: implementar UPDATE - actualizar libro

--- src/main/java/logica/LibroService.java
+++ src/main/java/logica/LibroService.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logica;
+import java.util.ArrayList;
+import modelos.Libro;
+import persistencia.LibroRepository;
 
-/**
- *
- * @author HP
- */
 public class LibroService {
-    
+    private LibroRepository repository;
+
+    public LibroService() {
+        this.repository = new LibroRepository();
+    }
+
+    public void crearLibro(String isbn, String titulo, String autor, String genero, int anioPublicacion, boolean disponible) {
+        Libro libro = new Libro(isbn, titulo, autor, genero, anioPublicacion, disponible);
+        repository.crearLibro(libro);
+    }
+
+    public void listarLibros() {
+        repository.listarLibros();
+    }
+
+    public Libro buscarLibro(String isbn) {
+        return repository.buscarLibro(isbn);
+    }
+
+    public void modificarLibro(String isbn, String nuevoTitulo, String nuevoAutor,
+                                String nuevoGenero, int nuevoAnio, boolean disponible) {
+        Libro libro = repository.buscarLibro(isbn);
+        if (libro != null) {
+            libro.setTitulo(nuevoTitulo);
+            libro.setAutor(nuevoAutor);
+            libro.setGenero(nuevoGenero);
+            libro.setAnioPublicacion(nuevoAnio);
+            libro.setDisponible(disponible);
+            repository.modificarLibro(libro);
+        } else {
+            System.out.println("No se puede modificar, libro no encontrado.");
+        }
+    }
 }
--- src/main/java/persistencia/LibroRepository.java
+++ src/main/java/persistencia/LibroRepository.java
@@ -1,32 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package persistencia;
 import java.util.ArrayList;
 import modelos.Libro;
-/**
- *
- * @author HP
- */
-public class LibroRepository {
-     private final LibroRepository repository;
-     
-     public LibroRepository(){
-         this.repository = new LibroRepository();
-     }
-     
-     public void crearLibro(Libro libro){
-         repository.crearLibro(libro);
-     }
-     
-     public void listarLibros(){
-         repository.listarLibros();
-     }
-     
-     
-     
-    
-    
-}           
 
+public class LibroRepository {
+    private ArrayList<Libro> libros;
+
+    public LibroRepository() {
+        this.libros = new ArrayList<>();
+    }
+
+    public void crearLibro(Libro libro) {
+        libros.add(libro);
+        System.out.println("Libro agregado correctamente.");
+    }
+
+    public void listarLibros() {
+        if (libros.isEmpty()) {
+            System.out.println("No hay libros registrados.");
+        } else {
+            for (Libro l : libros) {
+                System.out.println(l);
+            }
+        }
+    }
+
+    public Libro buscarLibro(String isbn) {
+        for (Libro l : libros) {
+            if (l.getIsbn().equals(isbn)) {
+                return l;
+            }
+        }
+        System.out.println("Libro no encontrado.");
+        return null;
+    }
+
+    public void modificarLibro(Libro libro) {
+        for (int i = 0; i < libros.size(); i++) {
+            if (libros.get(i).getIsbn().equals(libro.getIsbn())) {
+                libros.set(i, libro);
+                System.out.println("Libro modificado correctamente.");
+                return;
+            }
+        }
+        System.out.println("Libro no encontrado.");
+    }
+
+
+ public void eliminarLibro(String isbn) {
+        for (int i = 0; i < libros.size(); i++) {
+            if (libros.get(i).getIsbn().equals(isbn)) {
+                libros.remove(i);
+                System.out.println("Libro eliminado correctamente.");
+                return;
+            }
+        }
+        System.out.println("Libro no encontrado.");
+    }
+}
